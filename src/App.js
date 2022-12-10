d442afa1519ae28f6ec1a188d26f3a5063b36ddc
Denis Beresnev <shanthi.bunch@gmail.com>
2022-12-10 23:58:56 +0300

    grid-basics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Technologies from './Technologies';
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          className="img"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/VK_Compact_Logo_%282021-present%29.svg/2048px-VK_Compact_Logo_%282021-present%29.svg.png"
+          alt=""
+        />
+      </header>
+      <div className="left-block">
+        <nav className="nav">
+          <div>Моя страница</div>
+          <div>Новости</div>
+          <div>Мессенджер</div>
+          <div>Друзья</div>
+          <div>Сообщества</div>
+          <div>Фотографии</div>
+          <div>Друзья</div>
+        </nav>
+      </div>
+      <div className="content">
+        <div className="new_post">Что у вас нового?</div>
+        <div className="post">Пост</div>
+      </div>
+      <div className="right-block">
+        <nav className="nav">Правое меню</nav>
+        <div className="prometheus">Сначала интересное</div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
