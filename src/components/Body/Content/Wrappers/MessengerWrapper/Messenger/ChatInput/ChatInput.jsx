eb1a2e73b7104e6214b03610f5821d2c9f4a1dee
Denis Beresnev <shanthi.bunch@gmail.com>
2022-12-27 19:36:34 +0300

    onClick-ref

--- src/components/Body/Content/Wrappers/MessengerWrapper/Messenger/ChatInput/ChatInput.jsx
+++ src/components/Body/Content/Wrappers/MessengerWrapper/Messenger/ChatInput/ChatInput.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import s from "./ChatInput.module.css";
 
 const ChatInput = () => {
-    return <input type="text" />;
+    let newMessageRef = React.createRef();
+    const newMessage = () => alert(newMessageRef.current.value);
+    return (
+        <div className={s.chatInput}>
+            <input ref={newMessageRef} className={s.input} type="text" />
+            <button onClick={newMessage}>send</button>
+        </div>
+    );
 };
 
 export default ChatInput;
