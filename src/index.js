b1efb3cf32a129f0d29ab7ec65bdbb3c9a79e98f
Denis Beresnev <shanthi.bunch@gmail.com>
2023-01-01 02:59:37 +0300

    add-store
    rename rerenderEntireTree to callSubscribers
    private state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import { state, newMessage, updateChatInput } from "./redux/state";
-import { subscribers } from "./redux/state";
+import store from "./redux/state";
+import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,14 +11,14 @@ let rerenderEntireTree = (state) => {
         <React.StrictMode>
             <BrowserRouter>
                 <App
-                    state={state}
-                    newMessage={newMessage}
-                    updateChatInput={updateChatInput}
+                    state={store.getState().users.shanth1}
+                    newMessage={store.newMessage.bind(store)}
+                    updateChatInput={store.updateChatInput.bind(store)}
                 />
             </BrowserRouter>
         </React.StrictMode>,
     );
 };
 
-rerenderEntireTree(state.users.shanth1);
-subscribers(rerenderEntireTree);
+rerenderEntireTree(store.getState().users.shanth1);
+store.subscribers(rerenderEntireTree);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,132 +1,134 @@
 let dateStart = new Date();
 
-export const state = {
-    users: {
-        shanth1: {
-            passInfo: {
-                phone: "79048483502",
-                email: "shanthi.bunch@gmail.com",
-                password: "qwe123",
-            },
-            portfolio: {
-                name: "Denis",
-                surname: "Beresnev",
-                company: "Company Name",
-                posts: [
-                    { id: 1, title: "hi", bg: "bg", content: "content" },
-                    { id: 2, title: "hi", bg: "bg", content: "content" },
-                    { id: 3, title: "hi", bg: "bg", content: "content" },
-                ],
-            },
-            messages: {
-                dialogs: [
-                    {
-                        companion: "mark_orlov",
-                        messages: [
-                            {
-                                id: 1,
-                                text: "Привет",
-                                dateInfo: {
-                                    hours: dateStart.getHours(),
-                                    minutes: dateStart.getMinutes(),
-                                },
-                                sender: "shanth1",
-                            },
-                            {
-                                id: 2,
-                                text: "Привет!",
-                                dateInfo: {
-                                    hours: dateStart.getHours(),
-                                    minutes: dateStart.getMinutes(),
-                                },
-                                sender: "mark_orlov",
-                            },
-                            {
-                                id: 3,
-                                text: "Как дела?",
-                                dateInfo: {
-                                    hours: dateStart.getHours(),
-                                    minutes: dateStart.getMinutes(),
-                                },
-                                sender: "mark_orlov",
-                            },
-                        ],
-                        input: "Test",
-                    },
-                    {
-                        companion: "andaburas",
-                        messages: [
-                            {
-                                id: 1,
-                                text: "Здравствуйте!",
-                                date: new Date(),
-                                sender: "shanth1",
-                            },
-                            {
-                                id: 2,
-                                text: "Добрый вечер",
-                                date: new Date(),
-                                sender: "andabura",
-                            },
-                            {
-                                id: 3,
-                                text: "Вы кто?",
-                                date: new Date(),
-                                sender: "andabura",
-                            },
-                        ],
-                        input: "",
-                    },
-                ],
-            },
-            friends: ["mark_orlov", "sara_li", "oleg_lsp", "andabura"],
-            albums: {
-                photo: [],
-                video: [],
-                music: [],
-            },
-            music: [],
-            games: {
-                chess: {
-                    elo: 1200,
-                    history: {},
+let store = {
+    _state: {
+        users: {
+            shanth1: {
+                passInfo: {
+                    phone: "79048483502",
+                    email: "shanthi.bunch@gmail.com",
+                    password: "qwe123",
                 },
-                horror: {
-                    lvl: 1,
-                    rating: 10,
+                portfolio: {
+                    name: "Denis",
+                    surname: "Beresnev",
+                    company: "Company Name",
+                    posts: [
+                        { id: 1, title: "hi", bg: "bg", content: "content" },
+                        { id: 2, title: "hi", bg: "bg", content: "content" },
+                        { id: 3, title: "hi", bg: "bg", content: "content" },
+                    ],
+                },
+                messages: {
+                    dialogs: [
+                        {
+                            companion: "mark_orlov",
+                            messages: [
+                                {
+                                    id: 1,
+                                    text: "Привет",
+                                    dateInfo: {
+                                        hours: dateStart.getHours(),
+                                        minutes: dateStart.getMinutes(),
+                                    },
+                                    sender: "shanth1",
+                                },
+                                {
+                                    id: 2,
+                                    text: "Привет!",
+                                    dateInfo: {
+                                        hours: dateStart.getHours(),
+                                        minutes: dateStart.getMinutes(),
+                                    },
+                                    sender: "mark_orlov",
+                                },
+                                {
+                                    id: 3,
+                                    text: "Как дела?",
+                                    dateInfo: {
+                                        hours: dateStart.getHours(),
+                                        minutes: dateStart.getMinutes(),
+                                    },
+                                    sender: "mark_orlov",
+                                },
+                            ],
+                            input: "Test",
+                        },
+                        {
+                            companion: "andaburas",
+                            messages: [
+                                {
+                                    id: 1,
+                                    text: "Здравствуйте!",
+                                    date: new Date(),
+                                    sender: "shanth1",
+                                },
+                                {
+                                    id: 2,
+                                    text: "Добрый вечер",
+                                    date: new Date(),
+                                    sender: "andabura",
+                                },
+                                {
+                                    id: 3,
+                                    text: "Вы кто?",
+                                    date: new Date(),
+                                    sender: "andabura",
+                                },
+                            ],
+                            input: "",
+                        },
+                    ],
+                },
+                friends: ["mark_orlov", "sara_li", "oleg_lsp", "andabura"],
+                albums: {
+                    photo: [],
+                    video: [],
+                    music: [],
+                },
+                music: [],
+                games: {
+                    chess: {
+                        elo: 1200,
+                        history: {},
+                    },
+                    horror: {
+                        lvl: 1,
+                        rating: 10,
+                    },
                 },
             },
         },
     },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber(store) {},
+    subscribers(observer) {
+        this._callSubscriber = observer;
+    },
+    newMessage(text) {
+        let date = new Date();
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+
+        this._state.users.shanth1.messages.dialogs[0].messages.push({
+            id: 3,
+            text: text,
+            dateInfo: {
+                hours: hours,
+                minutes: minutes,
+            },
+            sender: "shanth1",
+        });
+        this._callSubscriber(this);
+    },
+    updateChatInput(newText) {
+        this._state.users.shanth1.messages.dialogs[0].input = newText;
+        this._callSubscriber(this);
+    },
 };
 
-let rerenderEntireTree = (state) => {
-    console.log("state changed");
-};
-
-export const subscribers = (observer) => {
-    rerenderEntireTree = observer;
-};
-
-export const newMessage = (text) => {
-    let date = new Date();
-
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-
-    state.users.shanth1.messages.dialogs[0].messages.push({
-        id: 3,
-        text: text,
-        dateInfo: {
-            hours: hours,
-            minutes: minutes,
-        },
-        sender: "shanth1",
-    });
-    rerenderEntireTree(state.users.shanth1);
-};
-
-export const updateChatInput = (newText) => {
-    state.users.shanth1.messages.dialogs[0].input = newText;
-    rerenderEntireTree(state.users.shanth1, newMessage, updateChatInput);
-};
+export default store;
+window.store = store;
